d54285a71f46a57f5069223a2949814af4834593
Harrison Gregg <harrison.gregg@ge.com>
2014-07-21 11:29:45 -0700

    Add Routing

---
+++ script.js
@@ -0,0 +1,17 @@
+angular.module('app', ['ngRoute'])
+
+.controller('HomeController', function($scope) {
+    $scope.links = [
+        {text:'About Me',url:'about'},
+        {text:'Projects',url:'projects'},
+        {text:'Blog',url:'blog'}
+    ];
+})
+
+.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'home.html',
+		controller: 'HomeController'
+	});
+});
