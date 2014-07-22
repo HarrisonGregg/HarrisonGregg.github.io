9aabae0cd32e0253eb55d897eef39b4d9bc4b02a
Harrison Gregg <harrison.gregg@ge.com>
2014-07-22 16:47:49 -0700

    Added project content

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ angular.module('app', ['ngRoute'])
         {text:'Projects',url:'projects'},
         {text:'Blog',url:'blog'}
     ];
+    $scope.projects = [
+    	{title: 'Spectracer', 			image:'spectracer.png', 		color: 'rgb(100, 47, 53)', url: 'spectracer',		body: 'This is an android app I made.'},
+    	{title: 'Spectracer in Unity', 	image:'spectracer-unity.png', 	color: 'rgb(100, 47, 53)', url: 'spectracer-unity',body: 'I started to remake Spectracer using Unity3D.'},
+    	{title: 'This Website', 		image:'', 		color: 'rgb(100, 47, 53)', url: 'this-site',		body: 'I made this website using AngualarJS.'},
+    	{title: 'Get Off My Lawn', 		image:'get-off-my-lawn.png', 	color: 'rgb(30, 100, 53)', url: 'get-off-my-lawn',	body: 'This is a mobile game that I started making in Unity3D.'},
+    	{title: 'Neural Net', 			image:'', 		color: 'rgb(100, 47, 53)', url: 'neural-net',		body: 'These are the things that I did with a neural net for my Artificial Intelligence class.'},
+    	{title: 'Curve Editor', 		image:'', 		color: 'rgb(30, 47, 100)', url: 'curve-editor',	body: 'This is a project for my Computer Graphics class.'},
+    	{title: 'Ray Tracer', 			image:'', 		color: 'rgb(100, 30, 100)', url: 'ray-tracer', 		body: 'This is a project for my Computer Graphics class.'},
+    	{title: 'Synthepyzer', 			image:'', 		color: 'rgb(100, 47, 53)', url: 'synthepyzer', 	body: 'This is a Python program that I made with my friends for HackPrinceton Fall 2013.'},
+    	{title: 'Mr. Burger', 			image:'mr-burger.png', 			color: 'rgb(200, 100, 0)', url: 'mr-burger', 		body: 'This is a silly flash game that I made with my friends.'},
+    	{title: 'Senior Thesis',		image:'bcsr.jpg', 				color: 'rgb(100, 100, 100)', url: 'senior-thesis', 	body: 'For my Senior Thesis at Bard College at Simon\'s Rock, I will be starting a business.'}
+    ];
 })
 
 .config(function($routeProvider) {
