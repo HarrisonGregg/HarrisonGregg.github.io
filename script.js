23102b23b7542acd4b58ccf0cdfd79f15bc15afa
Harrison Gregg <animasterh@gmail.com>
2014-09-24 13:27:25 -0400

    Added blog feature and first blog post.

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 projects = [
 	{title: 'Spectracer', 			image:'spectracer.png', 		color: 'rgb(100, 47, 53)', url: 'spectracer',		body: 'This is an android app I made.'},
 	{title: 'Spectracer in Unity', 	image:'spectracer-unity.png', 	color: 'rgb(100, 47, 53)', url: 'spectracer-unity', body: 'I started to remake Spectracer using Unity3D.'},
-	{title: 'This Website', 		image:'website.png', 			color: 'rgb(100, 47, 53)', url: 'this-site',		body: 'I made this website using AngualarJS.'},
-	{title: 'Get Off My Lawn', 		image:'get-off-my-lawn.png', 	color: 'rgb(30, 100, 53)', url: 'get-off-my-lawn',	body: 'This is a mobile game that I started making in Unity3D.'},
-	{title: 'Neural Net', 			image:'neural-nets.png', 		color: 'rgb(100, 47, 53)', url: 'neural-net',		body: 'These are the things that I did with a neural net for my Artificial Intelligence class.'},
-	{title: 'Curve Editor', 		image:'curve-editor.png', 		color: 'rgb(30, 47, 100)', url: 'curve-editor',		body: 'This is a project for my Computer Graphics class.'},
-	{title: 'Ray Tracer', 			image:'ray-tracer2.png', 		color: 'rgb(100, 30, 100)',url: 'ray-tracer', 		body: 'This is a project for my Computer Graphics class.'},
-	{title: 'Synthepyzer', 			image:'synthepyzer.png', 		color: 'rgb(100, 47, 53)', url: 'synthepyzer', 		body: 'This is a Python program that I made with my friends for HackPrinceton Fall 2013.'},
-	{title: 'Mr. Burger', 			image:'mr-burger.png', 			color: 'rgb(200, 100, 0)', url: 'mr-burger', 		body: 'This is a silly flash game that I made with my friends.'},
-	{title: 'Corona SDK Games', 	image:'corona.png', 			color: 'rgb(100, 100, 100)', url: 'corona', 		body: 'My first real mobile app programming was done using the Corona SDK.'},
-	{title: 'Senior Thesis',		image:'bcsr.jpg', 				color: 'rgb(100, 100, 100)', url: 'senior-thesis', 	body: 'For my Senior Thesis at Bard College at Simon\'s Rock, I will be starting a business.'}
+	{title: 'This Website', 		image:'website.png', 			color: 'rgb(100, 47, 53)', 	url: 'this-site',		body: 'I made this website using AngualarJS.'},
+	{title: 'Get Off My Lawn', 		image:'get-off-my-lawn.png', 	color: 'rgb(30, 100, 53)', 	url: 'get-off-my-lawn',	body: 'This is a mobile game that I started making in Unity3D.'},
+	{title: 'Neural Net', 			image:'neural-nets.png', 		color: 'rgb(100, 47, 53)', 	url: 'neural-net',		body: 'These are the things that I did with a neural net for my Artificial Intelligence class.'},
+	{title: 'Curve Editor', 		image:'curve-editor.png', 		color: 'rgb(30, 47, 100)', 	url: 'curve-editor',	body: 'This is a project for my Computer Graphics class.'},
+	{title: 'Ray Tracer', 			image:'ray-tracer2.png', 		color: 'rgb(100, 30, 100)',	url: 'ray-tracer', 		body: 'This is a project for my Computer Graphics class.'},
+	{title: 'Synthepyzer', 			image:'synthepyzer.png', 		color: 'rgb(100, 47, 53)', 	url: 'synthepyzer', 	body: 'This is a Python program that I made with my friends for HackPrinceton Fall 2013.'},
+	{title: 'Mr. Burger', 			image:'mr-burger.png', 			color: 'rgb(200, 100, 0)', 	url: 'mr-burger', 		body: 'This is a silly flash game that I made with my friends.'},
+	{title: 'Corona SDK Games', 	image:'corona.png', 			color: 'rgb(100, 100, 100)',url: 'corona', 			body: 'My first real mobile app programming was done using the Corona SDK.'},
+	{title: 'Senior Thesis',		image:'bcsr.jpg', 				color: 'rgb(100, 100, 100)',url: 'senior-thesis', 	body: 'For my Senior Thesis at Bard College at Simon\'s Rock, I will be starting a business.'}
+];
+
+posts = [
+	{title: 'Senior Thesis: Beginning the Journey', url: 'post1',	date:'September 17, 2014', 	datestamp:'2014-09-17', description: "This is the first of what will be weekly updates on my Senior Thesis.  I've decided on my thesis project, and turned in my thesis proposal.  I'm preparing to do market research. This post includes a description of my project, and where I imagine it going."},
+	{title: 'Senior Thesis: Blog, JIRA, and HRRC', 	url: 'post2',	date:'September 24, 2014', 	datestamp:'2014-09-24', description: "This past week, I've set up a basic version of my blog, so I can actually post these updates, I've started setting up JIRA to use for project organization, and I'm just waiting on someone to get back to me about the Human Research Review Committee guidelines I need to follow before I can send out my survey."},
 ];
 
 angular.module('app', ['ngRoute'])
@@ -22,6 +27,7 @@ angular.module('app', ['ngRoute'])
     ];
 
     $scope.projects = projects;
+    $scope.posts = posts;
 
 })
 
@@ -31,6 +37,16 @@ angular.module('app', ['ngRoute'])
 	for(i = 0; i < projects.length; i++){
 		if(projects[i].url==$routeParams.projectId){
 			$scope.project = projects[i];
+		}
+	}
+})
+
+.controller('BlogController', function($scope, $routeParams) {
+	var i;
+    $scope.post = null;
+	for(i = 0; i < posts.length; i++){
+		if(posts[i].url==$routeParams.postId){
+			$scope.post = posts[i];
 		}
 	}
 })
@@ -56,6 +72,10 @@ angular.module('app', ['ngRoute'])
 	.when('/projects/:projectId', {
 		templateUrl: 'project.html',
 		controller: 'ProjectController'
+	})
+	.when('/blog/:postId', {
+		templateUrl: 'post.html',
+		controller: 'BlogController'
 	})
 	.otherwise({
 		redirectTo:'/'
