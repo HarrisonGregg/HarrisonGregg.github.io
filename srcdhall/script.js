8f50fa900f92d9c3f287ecb8e848d9d7d1d0a690
Harrison Gregg <animasterh@gmail.com>
2015-01-23 12:13:51 -0500

    Added synthepyzer video.

--- srcdhall/script.js
+++ srcdhall/script.js
@@ -2,16 +2,21 @@ meSpeak.loadConfig("mespeak_config.json");
 meSpeak.loadVoice('en.json');
 
 // recognition.start();
+var speech = false;
+var id;
+
+var recognition = new webkitSpeechRecognition();
+recognition.onresult = function(event) { 
+	document.getElementById("listening").style.visibility = "hidden";
+  	document.getElementById("text").value = event.results[0][0].transcript;
+  	onSubmit();
+}
 
 function recognize(success){
-	if(success){
-		var recognition = new webkitSpeechRecognition();
-		recognition.onresult = function(event) { 
-		  	document.getElementById("text").value = event.results[0][0].transcript;
-		  	onSubmit();
-		}
+	if(success && speech){
 		recognition.start();
-		console.log("listening...");
+		// console.log("listening...");
+		document.getElementById("listening").style.visibility = "visible";
 	}
 }
 
@@ -23,7 +28,7 @@ function onSubmit(){
 	var state = document.getElementById("state").value;
 	var day = document.getElementById("day").value;
 	var meal = document.getElementById("meal").value;
-	var url = "https://young-plateau-8081.herokuapp.com/srcdhall?state="
+	var url = "srcdhall?state="
 	if(isNaN(parseInt(state))){
 		url += "0"
 	} else {
@@ -53,13 +58,24 @@ function onSubmit(){
 		document.getElementById("meal").value = responseObject.m;
 	}
 	if(responseObject.state < 6){
-		meSpeak.speak(responseObject.response, {}, recognize);
+		id = meSpeak.speak(responseObject.response, {}, recognize);
 	} else {
-		meSpeak.speak(responseObject.response);
+		id = meSpeak.speak(responseObject.response);
+		speech = false;
 	}
 }
 
+function startDialogue(){
+	document.getElementById("startDialogue").style.visibility = "hidden";
+	document.getElementById("stopDialogue").style.visibility = "visible";
+	speech = true;
+	onSubmit()	
+}
 
-onSubmit()
-
-
+function stopDialogue(){
+	document.getElementById("stopDialogue").style.visibility = "hidden";
+	document.getElementById("startDialogue").style.visibility = "visible";
+	speech = false;
+	meSpeak.stop(id);
+	recognition.stop();
+}
