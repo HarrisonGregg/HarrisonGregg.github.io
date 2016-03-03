4d69a9c35a999519325ad64940fd3cc9cd2ae081
Harrison Gregg <animasterh@gmail.com>
2016-03-03 15:16:03 -0500

    Added education project

---
+++ education-project/app/ts/post_list.component.ts
@@ -0,0 +1,54 @@
+import {Component, OnInit} from 'angular2/core';
+import {Post} from './post';
+import {PostService} from './post.service'
+
+@Component({
+	selector: 'post-list',
+	templateUrl: 'app/templates/post_list.component.html',
+	styleUrls: ['/app/css/post_list.component.html']
+})
+
+export class PostListComponent implements OnInit{
+	
+	public postlist : Post[];
+
+	constructor (private _postService: PostService) {
+		this.postlist = new Array<Post>();
+	}
+
+  	errorMessage: string;
+
+  	ngOnInit() { this.getPostList(); }
+  	getPostList() {
+  		console.log('component getpostlist called');
+    	this._postService.getPostList()
+                     	 .subscribe(
+                          	postlist => this.postlist = postlist,
+                          	error => this.errorMessage = <any>error);
+  	}
+
+
+	addPost(newLink = "http://google.com", newTitle = "title", newTopic=1) {
+		this._postService.addPost(newLink, newTitle, newTopic)
+						 .subscribe(
+                     		post => this.postlist.push(post),
+                     		error =>  this.errorMessage = <any>error);
+	}
+
+	upVote(post: Post){
+		post.score++;
+		this._postService.updateLink(post)
+						 .subscribe(
+						 	updatedPost => post = updatedPost,
+						 	error => this.errorMessage = <any>error);
+	}
+
+	downVote(post: Post){
+		post.score--;
+		this._postService.updateLink(post)
+						 .subscribe(
+						 	updatedPost => post = updatedPost,
+						 	error => this.errorMessage = <any>error);
+	}
+
+}
